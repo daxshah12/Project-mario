7ef89613a6553be3c501063c9d7e9ccd8787445c
daxshah12 <43219335+daxshah12@users.noreply.github.com>
2019-11-05 11:22:00 -0500

    Add files via upload

---
+++ PlaySound.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayCoinSound : MonoBehaviour {
+
+    public AudioSource coinsound;
+
+	// Use this for initialization
+	void Start () 
+{
+        coinsound = GetComponent<AudioSource>();
+	}
+	
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+
+     if(collision.gameObject.tag == "PickUp")
+        {
+            coinsound.Play();
+        }
+    }
+}
